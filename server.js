6bea71f8122fb390cdca7dca3e2c6741c67e1bb9
Rodrigo Emanuel <drigosantos@gmail.com>
2020-04-28 19:09:46 -0300

    Inserido layout e corrigido posicionamento do modal.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ nunjucks.configure("pages", {
     express: server,
     autoescape: false,
     noCache: true
-})
+});
 
 server.get("/", function(req, res) {
     return res.render("index");
@@ -20,7 +20,11 @@ server.get("/", function(req, res) {
 
 server.get("/sobre", function(req, res) {
     return res.render("sobre");
-})
+});
+
+server.get("/receitas", function(req, res) {
+    return res.render("receitas");
+});
 
 server.listen(5002, function() {
     console.log("Servidor ligado.");    
