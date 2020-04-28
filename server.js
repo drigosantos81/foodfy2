236d74b0f24fae1b989e56cd341542c871d28d1b
Rodrigo Emanuel <drigosantos@gmail.com>
2020-04-28 11:03:05 -0300

    Ajustado view engine(nunjucks'express: server'), endereço das rotas(index e sobre), script, css e imagens.

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
 server.use(express.static('public'));
 server.use(express.static('img'));
 
-server.set("view engine", "html");
+server.set("view engine", "njk");
+
+nunjucks.configure("pages", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
 
 server.get("/", function(req, res) {
-    return res.render("/index");
+    return res.render("index");
 });
+
+server.get("/sobre", function(req, res) {
+    return res.render("sobre");
+})
 
 server.listen(5002, function() {
     console.log("Servidor ligado.");    
