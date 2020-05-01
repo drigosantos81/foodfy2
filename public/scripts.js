efc2fa5c22d60e7d4fc861f10b51b84dd577f06e
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-01 20:33:52 -0300

    Implementação do click em receita para página prato.

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,31 @@
 const modal = document.querySelector('.modal');
 const grids = document.querySelectorAll('.receita');
+const gridsPr = document.querySelectorAll('.receitaPr');
 
-for (let grid of grids) {
-    grid.addEventListener("click", function() {
-        const imageId = grid.getAttribute("id");
-        const titulo = grid.querySelector("h3").innerHTML;
-        const dono = grid.querySelector("p").innerHTML;
+function modal() {
+    for (let grid of grids) {
+        grid.addEventListener("click", function() {
+            const imageId = grid.getAttribute("id");
+            const titulo = grid.querySelector("h3").innerHTML;
+            const dono = grid.querySelector("p").innerHTML;
 
-        modal.classList.add("active");
-        
-        modal.querySelector("img").src=`/${imageId}.png`;
-        modal.querySelector("h3").innerHTML = `${titulo}`;
-        modal.querySelector("p").innerHTML = `${dono}`;
-     })
+            modal.classList.add("active");
+            
+            modal.querySelector("img").src=`/${imageId}.png`;
+            modal.querySelector("h3").innerHTML = `${titulo}`;
+            modal.querySelector("p").innerHTML = `${dono}`;
+        });
+    }
+
+    document.querySelector(".close").addEventListener("click", function(event) {
+        event.preventDefault(event);
+        modal.classList.remove("active");
+    });
 }
 
-document.querySelector(".close").addEventListener("click", function(event) {
-    event.preventDefault(event)
-    modal.classList.remove("active")
-})
+for (let gridPr of gridsPr) {
+    gridPr.addEventListener("click", function() {
+        const pratoId = gridPr.getAttribute("id");
+        window.location.href = `/prato?id=${pratoId}`;
+    });
+}
