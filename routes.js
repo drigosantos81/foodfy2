10765be56976fcaa4b4f6c5d3957cb45cb9b8a18
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-12 09:13:14 -0300

    Estilizado page 'index' do Admin.

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const front = require('./controllers/front');
+const admin = require('./controllers/admin');
 
 // HOME/START
 routes.get("/", function(req, res) {
@@ -12,16 +13,22 @@ routes.get("/frontend", front.index);
 routes.get("/sobre", front.sobre);
 routes.get("/receitas", front.receitas);
 routes.get("/prato/:id", front.prato);
-
 routes.use("/not-found", front.notFound);
+
+//ADMIN
+routes.get("/admin", admin.index);
+
+
+// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+// routes.put("/admin/recipes", recipes.put); // Editar uma receita
+// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+
+module.exports = routes;
 
 // routes.use(function(req, res) {
 //     res.status(404).render("/frontend/not-found");
 // });
-
-//ADMIN
-routes.get("/admin", function(req, res) {
-    return res.render("admin/index");
-});
-
-module.exports = routes;
