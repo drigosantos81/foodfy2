a7c6bc46cc1c3f868a330a71b433dd91bdd430c3
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-12 19:54:24 -0300

    Rota para edição do prato.

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,21 @@ exports.exibe = function(req, res) {
     }
 
     return res.render("admin/prato", { item: receita });
-}
+};
+
+exports.edita = function(req, res) {
+    const receitaIndex = req.params.id;
+
+    const receita = receitas.find(function(receita) {
+        return receitaIndex == receita.id;
+    });
+
+    if (!receita) {
+        return res.render("frontend/not-found");
+    }
+
+    return res.render("admin/editar", { item: receita });
+};
 
 exports.notFound = function(req, res) {
     res.status(404).render("/frontend/not-found");
--- routes.js
+++ routes.js
@@ -18,9 +18,9 @@ routes.use("/not-found", front.notFound);
 //ADMIN
 routes.get("/admin", admin.index);
 routes.get("/admin/prato/:id", admin.exibe); // Exibir detalhes de uma receita
+routes.get("/admin/prato/:id/edit", admin.edita); // Mostrar formulário de edição de receita
 
 // routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
 // routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 // routes.put("/admin/recipes", recipes.put); // Editar uma receita
